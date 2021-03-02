b76bc849bd7888c0141e2fa3b0eb90f98666f7da
minh_coding <ngocminh8798@gmail.com>
2021-03-02 21:47:03 +0700

    update page top

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-(function($) {
+(function ($) {
 	"use strict"
 
 	// Fixed Nav
 	var lastScrollTop = 0;
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var wScroll = $(this).scrollTop();
-		if ( wScroll > $('#nav').height() ) {
-			if ( wScroll < lastScrollTop ) {
+		if (wScroll > $('#nav').height()) {
+			if (wScroll < lastScrollTop) {
 				$('#nav-fixed').removeClass('slide-up').addClass('slide-down');
+				$('#nav')
 			} else {
 				$('#nav-fixed').removeClass('slide-down').addClass('slide-up');
 			}
@@ -25,9 +26,9 @@
 	});
 
 	// Aside Nav
-	$(document).click(function(event) {
+	$(document).click(function (event) {
 		if (!$(event.target).closest($('#nav-aside')).length) {
-			if ( $('#nav-aside').hasClass('active') ) {
+			if ($('#nav-aside').hasClass('active')) {
 				$('#nav-aside').removeClass('active');
 				$('#nav').removeClass('shadow-active');
 			} else {
@@ -46,16 +47,16 @@
 
 	// Sticky Shares
 	var $shares = $('.sticky-container .sticky-shares'),
-	$sharesHeight = $shares.height(),
-	$sharesTop,
-	$sharesCon = $('.sticky-container'),
-	$sharesConTop,
-	$sharesConleft,
-	$sharesConHeight,
-	$sharesConBottom,
-	$offsetTop = 80;
+		$sharesHeight = $shares.height(),
+		$sharesTop,
+		$sharesCon = $('.sticky-container'),
+		$sharesConTop,
+		$sharesConleft,
+		$sharesConHeight,
+		$sharesConBottom,
+		$offsetTop = 80;
 
-	function setStickyPos () {
+	function setStickyPos() {
 		if ($shares.length > 0) {
 			$sharesTop = $shares.offset().top
 			$sharesConTop = $sharesCon.offset().top;
@@ -65,27 +66,70 @@
 		}
 	}
 
-	function stickyShares (wScroll) {
+	function stickyShares(wScroll) {
 		if ($shares.length > 0) {
-			if ( $sharesConBottom - $sharesHeight - $offsetTop < wScroll ) {
-				$shares.css({ position: 'absolute', top: $sharesConHeight - $sharesHeight , left:0});
-			} else if ( $sharesTop < wScroll + $offsetTop ) {
-				$shares.css({ position: 'fixed', top: $offsetTop, left: $sharesConleft });
+			if ($sharesConBottom - $sharesHeight - $offsetTop < wScroll) {
+				$shares.css({
+					position: 'absolute',
+					top: $sharesConHeight - $sharesHeight,
+					left: 0
+				});
+			} else if ($sharesTop < wScroll + $offsetTop) {
+				$shares.css({
+					position: 'fixed',
+					top: $offsetTop,
+					left: $sharesConleft
+				});
 			} else {
-				$shares.css({position: 'absolute', top: 0, left: 0});
+				$shares.css({
+					position: 'absolute',
+					top: 0,
+					left: 0
+				});
 			}
 		}
 	}
 
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		stickyShares($(this).scrollTop());
 	});
 
-	$(window).resize(function() {
+	$(window).resize(function () {
 		setStickyPos();
 		stickyShares($(this).scrollTop());
 	});
 
 	setStickyPos();
-
+	$("#pageTop a").addClass('active');
+	$("#pageTop").on("click", function (e) {
+		e.preventDefault();
+		$("html, body").animate({
+			scrollTop: 0
+		}, 1000);
+	})
+	$(window).on("load scroll resize", function () {
+		var wHeight = $(this).height();
+		var pFootter = $("footer").offset().top;
+		var scrollTop = $(this).scrollTop();
+		var hWin = wHeight + scrollTop;
+		if ($(window).width() < 767) {
+			pFootter = pFootter;
+		}
+		if (scrollTop <= 0) {
+			$("#pageTop").css({
+				"opacity": "0",
+				"pointer-events": "none"
+			});
+		} else {
+			$("#pageTop").css({
+				"opacity": "1",
+				"pointer-events": "all"
+			});
+		}
+		if (hWin <= pFootter) {
+			$("#pageTop").addClass('active');
+		} else {
+			$("#pageTop").removeClass('active');
+		}
+	});
 })(jQuery);
